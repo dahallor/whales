Fixed AVG Pool backprop

pool.py
@@ -19,15 +19,15 @@ class PoolingLayer():
         self.size_pooled = []
         self.size_original = []
         self.pool = np.array([])
+        self.pool_size = 2
+        self.stride = 2
 
     def pooling(self, image, pool_type):
         self.size_original.append(image.shape[0])
         self.size_original.append(image.shape[1])
-        pool_size = 2
-        stride = 2
-        for i in np.arange(image.shape[0], step = stride):
-            for j in np.arange(image.shape[0], step = stride):
-                temp = image[i:i+pool_size, j:j+pool_size]
+        for i in np.arange(image.shape[0], step = self.stride):
+            for j in np.arange(image.shape[0], step = self.stride):
+                temp = image[i:i+self.pool_size, j:j+self.pool_size]
                 if pool_type == "max":
                     line1 = max(temp[0][0], temp[0][1])
                     line2 = max(temp[1][0], temp[1][1])
@@ -49,15 +49,12 @@ class PoolingLayer():
                     line1 = np.sum(temp[0], dtype = 'float')
                     line2 = np.sum(temp[1], dtype = 'float')
                     total = line1 + line2
-                    avg_val = total / (pool_size * pool_size)
+                    avg_val = total / (self.pool_size * self.pool_size)
                     self.pool = np.append(self.pool, avg_val)
         
         self.pool = self.pool.reshape(image.shape[0]//2, image.shape[1]//2)
         self.size_pooled.append(image.shape[0])
         self.size_pooled.append(image.shape[1])
-        print(image)
-        print(self.pool)
-        print(self.coordinates)
         return self.pool
 
 
@@ -85,17 +82,13 @@ class PoolingLayer():
                     gradientOut[position1][position2] = value
                     k += 1
         if self.pool_type == "avg":
-            line = self.size_original[0] * self.size_original[1]
-            gradientOut = gradientOut.reshape(1, line)
-            print(gradientOut)
-            for i in range(gradientIn.shape[0]):
-                for j in range(gradientIn.shape[1]):
-                    value = gradientIn[i][j]/4
-                    for a in range(4):
-                        gradientOut[0][k] = value
-                        k += 1
-        gradientOut = gradientOut.reshape(self.size_original[0], self.size_original[1])
-        print(gradientOut)
+            line = gradientIn.shape[0] * gradientIn.shape[1]
+            gradientIn = gradientIn.reshape(line)
+            for i in np.arange(gradientOut.shape[0], step = self.stride):
+                for j in np.arange(gradientOut.shape[0], step = self.stride):
+                    value = gradientIn[k]/(self.pool_size * self.pool_size)
+                    gradientOut[i:i+self.pool_size, j:j+self.pool_size] = value
+                    k += 1
         return gradientOut
 
 

test_pool.py
@@ -4,7 +4,8 @@ from pool import *
 
 class TestPoolingLayer(unittest.TestCase):
 
-    test_array = np.array([
+    def __init__(self):
+        self.test_array = np.array([
         [1, 2, 3, 4, 5, 6],
         [6, 5, 4, 3, 2, 1],
         [1, 3, 5, 7, 9, 11],
@@ -13,19 +14,15 @@ class TestPoolingLayer(unittest.TestCase):
         [9, 9, 9, 9, 9, 9]
     ])
 
+    PL = PoolingLayer()
+
+
+
     def test_MaxPool(self):
-        test_array = np.array([
-        [1, 2, 3, 4, 5, 6],
-        [6, 5, 4, 3, 2, 1],
-        [1, 3, 5, 7, 9, 11],
-        [2, 4, 6, 8, 10, 12],
-        [1, 1, 1, 1, 1, 1],
-        [9, 9, 9, 9, 9, 9]
-    ])
         expected = [[6.0, 4.0, 6.0],
         [4.0, 8.0, 12.0],
         [9.0, 9.0, 9.0]]
-        self.assertEqual(max_pool(test_array), expected)
+        self.assertEqual(PL.max_pool(self.test_array), expected)
     '''
     def test_AvgPool(self, test_array):
         expected = [[3.5, 3.5, 3.5],