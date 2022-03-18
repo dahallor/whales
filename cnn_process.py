cf9e942444067dc568dd6fb10e440bcdb145c4c1
Philinphiladelphia <41462547+Philinphiladelphia@users.noreply.github.com>
2022-03-17 21:51:32 -0400

    Update cnn_process.py

--- cnn_process.py
+++ cnn_process.py
@@ -6,108 +6,123 @@ from activations import *
 from main import MulticlassNetwork
 from pool import *
 
-def setup():
-	# train_percent is a float in range [0,1] indicating how much of the data to allocate to training vs validation
-	TRAIN_PERCENT = 2/3
+LEARNING_RATE = 0.00001
+MAX_EPOCHS = 100
+BATCH_SIZE = 10
 
-	observations = []
-	predictions = []
-	skip = True
-	with open("KidCreative.csv", "r") as dataset:
-		# Skip header row
-		for row in dataset:
-			if skip:
-				skip = False
-				continue
-			row = row.split(",")
-			convertedRow = [float(x) for x in row]
-			observations.append(convertedRow[2:len(convertedRow)])
-			predictions.append(convertedRow[1])
+class MulticlassNetwork:
+	def __init__(self, train_set, objective, learning_rate=LEARNING_RATE, max_epochs=MAX_EPOCHS, batch_size=BATCH_SIZE):
+		self.learning_rate = learning_rate
+		self.max_epochs = max_epochs
+		self.batch_size = batch_size
 
-	train_num = math.ceil(len(observations) * TRAIN_PERCENT)
+		self.middle_layers = middle_layers
+		self.objective = objective
 
-	shuffled_indices = random.sample(range(len(observations)), len(observations))
+		self.setup(train_set)
 
-	self.train_set = np.array([observations[x] for x in shuffled_indices[0:train_num-1]])
-	self.train_pred = np.array([predictions[x] for x in shuffled_indices[0:train_num-1]])
+	def one_hot_encode(self, predictions):
+		n_values = np.max(predictions) + 1
+		return np.eye(int(n_values))[predictions]
 
-	self.val_set = np.array([observations[x] for x in shuffled_indices[train_num-1:len(observations)]])
-	self.val_pred = np.array([predictions[x] for x in shuffled_indices[train_num-1:len(observations)]])
+	def setup(self, train_set):
+		shuffled_train = random.sample(range(len(train_set)), len(train_set))
+		shuffled_val = random.sample(range(len(train_set)), len(train_set))
 
-	self.MIN_MAPE = 10**-10
-	self.MAX_EPOCHS = 10000
-	self.LEARNING_RATE = 10**-4
+		self.train_set = np.array([train_set[x] for x in shuffled_train])
+		self.train_pred = self.one_hot_encode([int(train_pred[x]) for x in shuffled_train])
 
-	self.input = InputLayer(self.train_set)
-	self.hidden1 = FullyConnectedLayer(16,1)
-	self.activation = SigmoidLayer()
-	self.objective = LogLoss()
+		self.val_set = np.array([val_set[x] for x in shuffled_val])
+		self.val_pred = self.one_hot_encode([int(val_pred[x]) for x in shuffled_val])
 
-	self.epoch = 0
-	self.train_ll = []
-	self.val_ll = []
+		self.input_layer = InputLayer(self.train_set)
 
-def learn(self):
+	def learn(self):
+		self.epoch = 0
+		epochs = []
+		train_accs = []
+		val_accs = []
 
-	mape = math.inf
+		while (self.epoch < self.max_epochs):
+			acc, obj = self.train_epoch(self.train_set, self.train_pred, self.batch_size)
+			val_acc, val_obj = self.validate_epoch(self.val_set, self.val_pred)
 
-	while (self.epoch < self.MAX_EPOCHS and mape > self.MIN_MAPE):
-		ll, acc = self.train_epoch()
-		self.train_ll.append(ll)
-		val_ll, val_acc = self.validate_epoch()
-		self.val_ll.append(val_ll)
+			if self.epoch % 100 == 0:
+				print(f"Completed epoch {self.epoch}. Train Acc: {acc}. Val Acc: {val_acc}")
+			self.epoch += 1
 
-		self.epoch += 1
+			acc, obj = self.train_epoch(self.train_set, self.train_pred, self.batch_size)
+			val_acc, val_obj = self.validate_epoch(self.val_set, self.val_pred)
 
-	plt.plot(range(self.epoch), self.train_ll, label="Training")
-	plt.plot(range(self.epoch), self.val_ll, label="Validation")
-	plt.xlabel("Epochs")
-	plt.ylabel("Log Loss")
-	plt.legend()
-	plt.show()
+			train_accs.append(obj)
+			val_accs.append(val_obj)
+			epochs.append(self.epoch)
 
-	print(f"Training Accuracy: {acc}, Validation Accuracy: {val_acc}")
+			if epoch % 100 == 0:
+				print(f"Completed epoch {epoch}. Train Acc: {acc}. Val Acc: {val_acc}")
+			epoch += 1
 
-def train_epoch(self):
-	l1 = self.input.forward(self.train_set)
-	l2 = self.hidden1.forward(l1)
-	l3 = self.activation.forward(l2)
-	l4 = self.objective.eval(self.train_pred, l3)
+		plt.xlabel("Epochs")
+		plt.ylabel("Log Loss")
+		plt.plot(epochs, train_accs, label = 'Training Objective', color = "black")
+		plt.plot(epochs, val_accs, label = 'Validation Objective', color = "black")
+		plt.legend()
+		plt.show()
 
-	g1 = self.objective.gradient(self.train_pred, l3) #TODO check
-	g2 = self.activation.backward(g1)
-	g3 = self.hidden1.backward(g2, self.LEARNING_RATE)
+		print(f"Training Accuracy: {acc}, Validation Accuracy: {val_acc}")
 
-	correct = 0
-	for i in range(len(l3)):
-		pred = 0
-		if l3[i] < 0.5:
-			pred = 1
-		else:
-			pred = 0
+	def get_multiclass_accuracy(self, y, yhat):
+		return np.sum(np.argmax(yhat, axis=1) == np.argmax(y, axis=1)) / len(y)
 
-		if pred == self.train_pred[i]:
-			correct+=1
+	def forward_prop(self, train_set, train_pred):
+		input = self.input_layer.forward(train_set)
+		for layer in self.middle_layers:
+			input = layer.forward(input)
+		return self.objective.eval(train_pred, input), input
+		
+	def backprop(self, train_pred, obj):
+		grad = self.objective.gradient(train_pred, obj)
+		for layer in reversed(self.middle_layers):
+			grad = layer.backward(grad, LEARNING_RATE)
 
-	accuracy = correct / len(l3)
-	
-	return np.mean(l4), accuracy
+	def minibatch(self, train_set, train_pred, batchsize):
+		indices = np.arange(train_set.shape[0])
+		np.random.shuffle(indices)
+		for start_idx in range(0, train_set.shape[0] - batchsize + 1, batchsize):
+			excerpt = indices[start_idx:start_idx + batchsize]
+			yield train_set[excerpt], train_pred[excerpt]
 
-def testWhaleCNN(images, targets):
-	input = InputLayer(images)
-	conv1 = ConvolutionalLayer()
-	activ1 = SigmoidLayer()
-	pool1 = PoolingLayer()
-	conv2 = ConvolutionalLayer()
-	activ2 = SigmoidLayer()
-	d1 = FullyConnectedLayer(9, 5)
-	drop1 = DropOutLayer()
-	d2 = FullyConnectedLayer(5,5)
-	activ3 = SoftmaxLayer()
+	def train_epoch(self, train_set, train_pred, batch_size):
+		acc_list = []
+		objs = []
+		for batch in self.minibatch(train_set, train_pred, batch_size):
+			curr_train, curr_pred = batch
+			obj, yhat = self.forward_prop(curr_train, curr_pred)
+			self.backprop(curr_pred, obj)
+			acc_list.append(self.get_multiclass_accuracy(curr_pred, yhat))
+			objs.append(obj)
+		return np.mean(acc_list), np.mean(objs)
+
+	def validate_epoch(self, val_set, val_pred):
+		obj, yhat = self.forward_prop(val_set, val_pred)
+		accuracy = self.get_multiclass_accuracy(val_pred, yhat)
+		return accuracy, obj
+
+def testWhaleCNN(test_set):
+	middle_layers = [
+	ConvolutionalLayer()
+	SigmoidLayer()
+	PoolingLayer()
+	ConvolutionalLayer()
+	SigmoidLayer()
+	FullyConnectedLayer(9, 5)
+	DropOutLayer()
+	FullyConnectedLayer(5,5)
+	SoftmaxLayer()
+		]
 
 	objective = CrossEntropy()
 
-
-
-
+	network = MulticlassNetwork(middle_layers, objective)
+	
 	network.learn()
